97f04bfd2336a1bbe2d3fd552e44eb9edf67b630
Lorenzo <baiyingpoi7z@gmail.com>
2024-10-17 14:29:22 +0800

    update

--- yoloe2ev2/build.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-rm -rf build
-mkdir build
-cd build
-cmake .. -DTRT_PATH=/home/ubuntu/TensorRT-8.5.2.2/
-make  -j$(nproc)
--- yoloe2ev2/python/yolov8/run_yolov8_nms.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-input_model=/home/jack/data1/project/yolov9/yolov9-m-converted.onnx
-outputmodel=/home/jack/data1/project/yolov9/yolov9-m-converted_1.onnx
-numcls=80
-
-python yolov8_add_postprocess.py --inputmodel ${input_model} --outputmodel ${outputmodel} --numcls ${numcls}
-python yolov8_add_nms.py --model ${outputmodel} --numcls ${numcls}
